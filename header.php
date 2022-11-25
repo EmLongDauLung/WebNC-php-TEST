c55c2f1d0157846e2441e31fe5f5277d2870f416
Long <you@example.com>
2022-11-26 01:08:29 +0700

    posts option

--- header.php
+++ header.php
@@ -9,11 +9,14 @@
     <script type="text/javascript" language="javascript" src="./assets/js/main.js"></script>
     <link rel="stylesheet" href="./assets/fontawesome-free-6.1.1/css/all.min.css">
     <link rel="stylesheet" href="./assets/css/posts.css">
-    <!-- <script src="https://localhost/Webdemo/assets/ckeditor5-build-decoupled-document/ckeditor.js"></script> -->
     <script src="./assets/ckeditor/ckeditor.js"></script>
     <!-- <link rel="stylesheet" href="../bootstrap-5.2.2-dist/css/bootstrap-grid.css"> -->
     <link rel="stylesheet" href="./assets/bootstrap-5.2.2-dist/css/bootstrap.css">
     <link rel="stylesheet" href="./assets/bootstrap-5.2.2-dist/js/bootstrap.js">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
     <link rel="icon" href="./assets/img/small_logo.png">
     <title>BOT STORE</title>
 </head>
--- profile.php
+++ profile.php
@@ -1,47 +1,73 @@
 <?php include("./header.php") ?>
-    <style>
-        .container__body{
-            width: 100%;
-            height: auto;
-        }
-        .container__profile{
-            width:  80%;
-            height: auto;
-            margin: 50px auto;
-        }
-        .container__profile__title{
-            width: fit-content;
-            height: auto;
-            padding: 4px 15px;
-            margin: 0 0 18px 0;
-            background-color: #397224;
-            color: white;
-            border-radius: 12px;
-            font-size: 20px;
-            font-weight: 700;
-        }
-    </style>
+<style>
+    .container__body {
+        width: 100%;
+        height: auto;
+    }
+
+    .container__profile {
+        width: 80%;
+        height: auto;
+        margin: 50px auto;
+    }
+
+    .container__profile__title {
+        width: fit-content;
+        height: auto;
+        padding: 4px 15px;
+        margin: 0 0 18px 0;
+        background-color: #397224;
+        color: white;
+        border-radius: 12px;
+        font-size: 20px;
+        font-weight: 700;
+    }
+
+    .option__posts {
+        color: #72af5c;
+        margin: 0 4px;
+    }
+
+    .option__posts:hover {
+        color: #397224;
+    }
+</style>
+
 <body class="container__body">
     <div class="container__profile">
         <div class="container__profile__title">Bài viết của bạn</div>
-        <?php 
-        	// Lấy ra nội dung bài viết theo điều kiện id
-            $sql = "select * from posts where user_id = '{$_SESSION['user_id']}'";
-            // Thực hiện truy vấn data thông qua hàm mysqli_query
-            $query = mysqli_query($conn,$sql);
-            while ( $data = mysqli_fetch_array($query) ) {
-                echo"
-                <table class='table table-bordered table-striped'>
-                    <td>
-                        <h3>{$data['title']}</h3> </br>
-                        <i> Ngày tạo: {$data['createdate']}</i>
-                        <p>{$data['content']}</p>
-                        <a href='showposts.php?id={$data['posts_id']}'> Xem thêm</a>
-                    </td>
-                </table>    
-                ";
-            }
-        ?>
+        <table class='table table-hover'>
+            <thead>
+                <tr>
+                    <th scope='col'>ID</th>
+                    <th scope='col'>Title</th>
+                    <th scope='col'>CreateDate</th>
+                    <th scope='col'>Content</th>
+                    <th scope='col'>Option</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $sql = "select * from posts where user_id = '{$_SESSION['user_id']}'";
+                $query = mysqli_query($conn, $sql);
+                while ($data = mysqli_fetch_array($query)) {
+                    echo "
+                    <tr>
+                        <th scope='row'>{$data['posts_id']}</th>
+                        <td>{$data['title']}</td>
+                        <td>{$data['createdate']}</td>
+                        <td>" . substr($data['content'], 0, 100) . "</td>
+                        <td>
+                            <a href='showposts.php?id={$data['posts_id']}' class='option__posts'><i class='fa-solid fa-eye'></i></a>
+                            <a href='fixposts.php?id={$data['posts_id']}' class='option__posts'><i class='fa-solid fa-pen-to-square'></i></a>
+                            <a href='deleteposts.php?id={$data['posts_id']}' class='option__posts'><i class='fa-solid fa-trash'></i> </a>
+                        </td>
+                    </tr>  
+                    ";
+                }
+                ?>
+            </tbody>
+        </table>
     </div>
 </body>
 <?php include("./footer.php") ?>
